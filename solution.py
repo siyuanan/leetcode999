c5c62427514703a9bdd6ffdfaa540113d515fb25
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-06-19 21:53:07 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -51,4 +51,36 @@ class Solution:
                 top = m1 + 1
         return searchRow(matrix[top-1], target)
 
-  # 
+    # 658
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if len(arr) == k: 
+            return arr
+
+        # binary search to get closest element
+        left = 0
+        right = len(arr)
+        while left < right - 1: 
+            print(arr[left: right + 1])
+            mid = (left + right) // 2
+            if arr[mid] == x:
+                left = mid
+                break
+            elif arr[mid] > x: 
+                right = mid
+            else: 
+                left = mid + 1
+
+        # left will be the index of closest value now
+        # expand from left
+        right = left
+        while right - left + 1 < k: 
+            if left == 0: 
+                right += 1
+            elif right == len(arr) - 1: 
+                left -= 1
+            elif abs(arr[right + 1] - x) < abs(arr[left - 1] - x):
+                right += 1
+            else: 
+                left -= 1
+
+        return arr[left:right+1]
