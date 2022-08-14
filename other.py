1ed4f2aeef7da88bfb3d99d999cc81f766cf1064
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-08-13 22:37:02 -0500

    Update other.py

--- other.py
+++ other.py
@@ -28,3 +28,37 @@ class Solution:
                 maxRight = currentRight - 1
         
         return s[maxLeft:maxRight + 1]
+
+    # 283 move 0s to the end
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1: 
+            return
+        # two pointer, will not maintain the relative order
+        # l = 0
+        # r = len(nums) - 1
+        # # from l to r (inclusive) is the unexplored area
+        # # from 0 to l-1 are the non-zeros
+        # # from r+1 to end are zeros
+        # while l <= r: 
+        #     if nums[l] == 0: 
+        #         nums[l] = nums[r]
+        #         nums[r] = 0
+        #         r -= 1
+        #     else: 
+        #         l += 1
+        
+        # two pointer go together
+        slow = 0
+        fast = 0
+        while fast < len(nums): 
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+                fast += 1
+            else: 
+                fast += 1
+        for i in range(slow, len(nums)): 
+            nums[i] = 0
