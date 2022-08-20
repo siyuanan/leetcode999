c1c60f702a084766dd21195f4fc8415c22d532d7
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-08-20 13:40:36 -0500

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -2,11 +2,18 @@
 class Solution:
     # 314. Binary Tree Vertical Order Traversal
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root == None: 
+            return []
+        
         q = deque([(root, 0)])
         table = dict()
+        min_col = 0
+        max_col = 0
         while q: 
             node, col = q.popleft()
             if node != None: 
+                min_col = min(col, min_col)
+                max_col = max(col, max_col)
                 if col not in table.keys(): 
                     table[col] = []
                     
@@ -15,4 +22,4 @@ class Solution:
                 q.append((node.left, col - 1))
                 q.append((node.right, col + 1))
                 
-        return [table[c] for c in sorted(table.keys())]
+        return [table[c] for c in range(min_col, max_col+1)]
