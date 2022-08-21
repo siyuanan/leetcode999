17f24e7f20e4c1e171b50096ab25cd79dcee3ec7
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-08-20 21:34:55 -0500

    Update other.py

--- other.py
+++ other.py
@@ -149,6 +149,23 @@ class Solution:
         # return pos
         return ''.join(s1)
     
+    # 921. Minimum Add to Make Parentheses Valid
+    def minAddToMakeValid(self, s: str) -> int:
+        if len(s) <= 0: 
+            return 0
+        left = 0
+        cnt = 0
+        for i in range(len(s)): 
+            if s[i] == '(': 
+                left += 1
+            else: 
+                left -= 1
+            if left < 0: 
+                cnt += 1
+                left += 1
+        cnt += left
+        return cnt
+    
     # 408. Valid Word Abbreviation
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         w = 0
