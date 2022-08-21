cd5108a34fa7e40c50444f8b08c1783bac92a020
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-08-20 23:05:52 -0500

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -23,3 +23,21 @@ class Solution:
                 q.append((node.right, col + 1))
                 
         return [table[c] for c in range(min_col, max_col+1)]
+    
+    # 339. Nested List Weight Sum
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        q = deque(nestedList)
+        s = 0
+        d = 1
+        while len(q) > 0: 
+            for i in range(len(q)): 
+                l = q.pop()
+                if l.isInteger(): 
+                    s += l.getInteger() * d
+                else: 
+                    # not append
+                    # append takes everything as single element
+                    # extend put in each element one by one
+                    q.extendleft(l.getList())
+            d += 1
+        return s
